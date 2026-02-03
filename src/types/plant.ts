79c9a3d700432c3cc99807348de3ce20bf9b1503
Esteban <eegnom1807@gmail.com>
2026-02-03 16:04:02 -0600

    add crud for plants

--- src/types/plant.ts
+++ src/types/plant.ts
@@ -1,21 +1,26 @@
-// export interface Plant {
-//   id: string
-//   name: string
-//   imageUrl: string
-//   humidity: number
-//   lastReadingDate: string
-// }
-
 export interface Plant {
   id: string;
   name: string;
-  species: string;
-  created_at: string;
+  image_url: string;
+  active: boolean;
   updated_at: string;
 }
 
 export interface PlantRequest {
   id?: string;
   name: string;
-  species: string;
+  image_url: string;
+}
+
+export interface PlantRequestUpdate {
+  name: string;
+  image_url: string;
+}
+
+export interface PlantsResponse {
+  data: Plant[];
+}
+
+export interface PlantResponse {
+  data: Plant;
 }
